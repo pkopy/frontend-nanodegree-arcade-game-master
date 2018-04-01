2a89ffa9e834dcf0a292d9f798510de4f564ead8
pkopy <pawel.kopycki@gmail.com>
2018-04-01 09:31:15 +0200

    Add class palyer and enemy, add player's control

--- js/app.js
+++ js/app.js
@@ -1,31 +1,103 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+// // Update the enemy's position, required method for game
+// // Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// // Draw the enemy on the screen, required method for game
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
 
+class Enemy {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/enemy-bug.png';
+    }
+    update(dt) {
+        
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+const enemy1 = new Enemy(0,0);
+const enemy2 = new Enemy(0,1);
+const enemy3 = new Enemy(0,2);
+const allEnemies = [enemy1, enemy2, enemy3]
+console.log(enemy1)
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor(look = 0, x, y) {
+        switch(look){
+            case 0:
+            this.look = 'images/char-boy.png';
+            break;
+            case 1:
+            this.look = 'images/char-cat-girl.png';
+            break;
+            case 2:
+            this.look = 'images/char-horn-girl.png';
+            break;
+            case 3:
+            this.look = 'images/char-pink-girl.png';
+            break;
+            case 4:
+            this.look = 'images/char-princess-girl.png';
+            break;
 
+        }
+        this.x = x;
+        this.y = y;
+        
+    }
+    update() {
 
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.look), this.x, this.y);
+
+    }
+    handleInput(input) {
+        switch(input) {
+            case 'left':
+            this.x -= 100;
+            this.render();
+            break;
+            case 'right':
+            this.x += 100;
+            this.render();
+            break;
+            case 'up':
+            this.y -= 85;
+            this.render();
+            break;
+            case 'down':
+            this.y += 85;
+            this.render();
+            break;
+        }
+
+    }
+}
+const player = new Player(2,100,400);
+console.log(player)
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -41,6 +113,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
+console.log(allowedKeys[e.keyCode])
     player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -173,8 +173,14 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
+
+
     Resources.onReady(init);
 
     /* Assign the canvas' context object to the global variable (the window
